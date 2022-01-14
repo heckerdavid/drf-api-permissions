8f99cba617e9a568ae3651a0fa33e21c2fe657fc
David Hecker <heckerdw@gmail.com>
2022-01-13 18:58:22 -0800

    Completed basic api setup with permissions.

--- write/views.py
+++ write/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from write.models import Write
+from .models import Write
+from .serializers import WriteSerializer
+from .permissions import IsAuthorOrReadOnly
+class WriteList(generics.ListCreateAPIView):
+    queryset = Write.objects.all()
+    serializer_class = WriteSerializer
+
+class WriteDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Write.objects.all()
+    serializer_class = WriteSerializer    
